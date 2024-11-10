5f1142dcc5aa82b7530d8a153ec9eeb2555b7c23
Your Name <you@example.com>
2024-11-10 23:38:59 +0300

    theme

--- script.js
+++ script.js
@@ -11,3 +11,18 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+const nightThemeBtn = document.getElementById("night-theme-btn")
+
+
+
+
+nightThemeBtn.addEventListener("click", function() {
+    document.body.classList.toggle("night-theme")
+
+    const theme = localStorage.getItem("theme")
+   if(theme === "night-theme") {
+    localStorage.setItem("theme", "")
+   }  else{
+    localStorage.setItem("theme", "night-theme")}
+})
