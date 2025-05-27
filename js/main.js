d9e49e150571648916bc15efe60dd0f5cf6aea8b
Your Name <you@example.com>
2025-05-27 21:26:43 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,13 @@
+import { initSmoothScroll } from './modules/smoothScroll.js';
+import { initThemeSwitcher } from './modules/themeSwitcher.js';
+import { initBurgerMenu } from './modules/burgerMenu.js';
+import { initModal } from './modules/modal.js';
+import { initFormSubmit } from './modules/formSubmit.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  initSmoothScroll();
+  initThemeSwitcher();
+  initBurgerMenu();
+  initModal();
+  initFormSubmit();
+});
